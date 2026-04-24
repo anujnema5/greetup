794d7a65158991e80fad5e493dbe7790951992c7
anujnema-rootlex <anuj.nema@rootlex.com>
2026-04-25 00:51:52 +0530

    Refactor Firebase configuration to use environment variables
    - Updated Firebase configuration to utilize environment variables for sensitive information, enhancing security and flexibility.
    - Removed hardcoded values for API keys and project identifiers, ensuring compliance with best practices for managing credentials.

--- client/src/lib/firebase/client-app.ts
+++ client/src/lib/firebase/client-app.ts
@@ -1,22 +1,12 @@
 import { getApp, getApps, initializeApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBC0HjJUPL1EXpX8gno3x4OdIuiYDN8dT4",
-  authDomain: "greetup-1dba5.firebaseapp.com",
-  projectId: "greetup-1dba5",
-  storageBucket: "greetup-1dba5.firebasestorage.app",
-  messagingSenderId: "882587955782",
-  appId: "1:882587955782:web:a08c6f94dbaf3b51901047",
-  measurementId: "G-TP8PT432JZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 function getOrInitApp(): FirebaseApp {
