4471ffe6e4fabaaa76a037f6274e138e03cd80a8
anujnema5 <anujnema5@users.noreply.github.com>
2026-07-14 00:53:23 +0530

    Remove start-dev.bat script as it is no longer needed for development setup.

--- start-dev.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-wt ^
-  new-tab --title "server" -d "%~dp0apps\server" -- bun run dev ^; ^
-  new-tab --title "client" -d "%~dp0apps\client" -- bun run dev ^; ^
-  new-tab --title "rtc-service" -d "%~dp0apps\rtc-service" -- bun run dev ^; ^
-  new-tab --title "matching-service" -d "%~dp0apps\matching-service" -- bun run dev
